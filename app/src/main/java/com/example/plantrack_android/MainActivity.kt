01668aaf38ea610dca7171257dc78d83a7698822
Samuel Ping <55605862+samuel-ping@users.noreply.github.com>
2022-01-07 01:14:09 -0500

    Add simple plant card

--- app/src/main/java/com/example/plantrack_android/MainActivity.kt
+++ app/src/main/java/com/example/plantrack_android/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.plantrack_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.plantrack_android.ui.theme.PlanTrackAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +24,48 @@ class MainActivity : ComponentActivity() {
             PlanTrackAndroidTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PlantCard(nickname = "Shrimp", scientificName = "Echeveria fleur blanc")
                 }
             }
         }
     }
 }
 
+//@Composable
+//private fun PlantCardGrid(plants: List<Plant>) {
+//
+//}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun PlantCard(nickname: String, scientificName: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        PlanTrackAndroidTheme {
+            Column(modifier = Modifier.padding(24.dp)) {
+                Text(
+                    text = "$nickname",
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.h5
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "$scientificName", style = MaterialTheme.typography.subtitle1)
+                Spacer(modifier = Modifier.width(8.dp))
+                OutlinedButton(
+                    onClick = { /* TODO */ }
+                ) {
+                    Text("Show more")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PlanTrackAndroidTheme {
-        Greeting("Android")
+        PlantCard(nickname = "Shrimp", scientificName = "Echeveria fleur blanc")
     }
 }
---
+++ app/src/main/java/com/example/plantrack_android/model/Plant.kt
@@ -0,0 +1,4 @@
+package com.example.plantrack_android.model
+
+class Plant {
+}
