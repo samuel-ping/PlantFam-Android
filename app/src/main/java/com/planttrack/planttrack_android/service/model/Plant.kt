2fcd552560027ab56f8e4342ef927ffe3b978ce8
Samuel Ping <55605862+samuel-ping@users.noreply.github.com>
2022-01-16 00:55:56 -0500

    Add plant now working- forgot to commit the edited plant schema file

--- app/src/main/java/com/planttrack/planttrack_android/service/model/Plant.kt
+++ app/src/main/java/com/planttrack/planttrack_android/service/model/Plant.kt
@@ -4,20 +4,31 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
 import io.realm.annotations.RealmField
+import io.realm.annotations.Required
 import org.bson.types.ObjectId
+import java.time.LocalDate
+import java.time.LocalDate.now
 import java.util.*
 
 @RealmClass
-open class Plant : RealmObject() {
-    @PrimaryKey @RealmField("_id") var _id : ObjectId = ObjectId()
-//    var partition: String? = null
+open class Plant(
+    nickname: String? = null,
+    commonName: String? = null,
+    scientificName: String? = null,
+    adoptionDate: Date = Date(),
+    adoptedFrom: String? = null,
+    isDeceased: Boolean? = null,
+    deceasedDate: Date? = null,
+    parent: Plant? = null
+) : RealmObject() {
+    @PrimaryKey @RealmField("_id") var id : ObjectId = ObjectId()
 
-    var nickname: String? = null
-    var commonName: String? = null
-    var scientificName: String? = null
-    var adoptionDate: Date = Date()
-    var adoptedFrom: String? = null
-    var isDeceased: Boolean? = null
-    var deceasedDate: Date = Date()
-    var parent: Plant? = null
+    var nickname: String? = nickname
+    var commonName: String? = commonName
+    var scientificName: String? = scientificName
+    @Required var adoptionDate: Date = adoptionDate
+    var adoptedFrom: String? = adoptedFrom
+    var isDeceased: Boolean? = isDeceased
+    var deceasedDate: Date? = deceasedDate
+    var parent: Plant? = parent
 }
