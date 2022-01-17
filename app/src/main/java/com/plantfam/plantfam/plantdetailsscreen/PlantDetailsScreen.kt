e0971105e3a3df365b7a05af6ee578544b1c2b5d
Samuel Ping <55605862+samuel-ping@users.noreply.github.com>
2022-01-17 04:17:45 -0500

    Add placeholder photo for plants

--- app/src/main/java/com/plantfam/plantfam/plantdetailsscreen/PlantDetailsScreen.kt
+++ app/src/main/java/com/plantfam/plantfam/plantdetailsscreen/PlantDetailsScreen.kt
@@ -1,17 +1,22 @@
 package com.plantfam.plantfam.plantdetailsscreen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
+import com.plantfam.plantfam.R
 
 @Composable
 fun PlantDetailsScreen(
@@ -20,6 +25,7 @@ fun PlantDetailsScreen(
     viewModel: PlantDetailsViewModel
 ) {
     val plant = viewModel.getPlant(plantId)
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -38,11 +44,24 @@ fun PlantDetailsScreen(
     ) {
         with(plant!!) {
             Column {
+                if(plant.coverPhoto == null) {
+                    val painter = rememberImagePainter(R.drawable.plant_image_placeholder)
+                    val painterState = painter.state
+
+                    AnimatedVisibility(visible = (painterState is ImagePainter.State.Loading)) {
+                        CircularProgressIndicator()
+                    }
+                    Image(
+                        painter = painter,
+                        contentDescription = "Placeholder image for your plant.",
+                        modifier = Modifier.size(128.dp).clip(CircleShape)
+                    )
+                }
                 Image(
                     painter = rememberImagePainter(
                         data = "g",
                         builder = {
-//                            placeholder(R.drawable.plant_image_placeholder)
+//                            placeholder()
                             transformations(CircleCropTransformation())
                         }
                     ),
