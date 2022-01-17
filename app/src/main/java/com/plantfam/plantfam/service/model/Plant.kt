4b3d5b256da6f5e4afb6b5d1cb4a005d85e8c498
Samuel Ping <55605862+samuel-ping@users.noreply.github.com>
2022-01-17 04:17:29 -0500

    Update plant schema to include progress photos

--- app/src/main/java/com/plantfam/plantfam/service/model/Plant.kt
+++ app/src/main/java/com/plantfam/plantfam/service/model/Plant.kt
@@ -1,5 +1,6 @@
 package com.plantfam.plantfam.service.model
 
+import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
@@ -7,6 +8,12 @@ import io.realm.annotations.RealmField
 import io.realm.annotations.Required
 import org.bson.types.ObjectId
 import java.util.*
+
+@RealmClass(embedded = true)
+open class ProgressPhoto(
+    var imageURL: String? = null,
+    var dateTaken: Date = Date()
+) : RealmObject()
 
 @RealmClass
 open class Plant(
@@ -18,7 +25,11 @@ open class Plant(
     var adoptedFrom: String? = null,
     var isDeceased: Boolean? = null,
     var deceasedDate: Date? = null,
-    var parent: Plant? = null
+    var parent: Plant? = null,
+    var coverPhoto: String? = null,
+    var photos: RealmList<ProgressPhoto> = RealmList()
 ) : RealmObject() {
-    @PrimaryKey @RealmField("_id") var id : ObjectId = ObjectId()
+    @PrimaryKey
+    @RealmField("_id")
+    var id: ObjectId = ObjectId()
 }
